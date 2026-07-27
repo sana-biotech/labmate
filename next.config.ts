4c3614b03061c6e6a7d1c1dd3f04748e818b9b31
Sana parveen <ssanaparveen078@gmail.com>
2026-07-27 07:13:54 -0700

    fix deployment

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
-
-export default nextConfig;
 };
 
 export default nextConfig;
